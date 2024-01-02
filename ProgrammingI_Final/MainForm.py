bab29394abfc97f91bf283cb5ed73cab1c53025b
zacryecrowe <143837780+zacryecrowe@users.noreply.github.com>
2024-01-02 13:42:41 -0600

    Distance Func Implimented

--- ProgrammingI_Final/MainForm.py
+++ ProgrammingI_Final/MainForm.py
@@ -1,6 +1,6 @@
-﻿import System.Drawing
+﻿import math
+import System.Drawing
 import System.Windows.Forms
-
 from System.Drawing import *
 from System.Windows.Forms import *
 
@@ -17,7 +17,12 @@ class MainForm(Form):
 		self._label1 = System.Windows.Forms.Label()
 		self._player = System.Windows.Forms.Label()
 		self._tscroll = System.Windows.Forms.Timer(self._components)
-		self._label2 = System.Windows.Forms.Label()
+		self._CabnetInv = System.Windows.Forms.Label()
+		self._Tspawn = System.Windows.Forms.Timer(self._components)
+		self._Cabnet = System.Windows.Forms.Label()
+		self._Radio = System.Windows.Forms.Label()
+		self._progressBar1 = System.Windows.Forms.ProgressBar()
+		self._DebugBox = System.Windows.Forms.Label()
 		self.SuspendLayout()
 		# 
 		# label1
@@ -43,20 +48,71 @@ class MainForm(Form):
 		self._tscroll.Interval = 10
 		self._tscroll.Tick += self.TscrollTick
 		# 
-		# label2
+		# CabnetInv
 		# 
-		self._label2.BackColor = System.Drawing.SystemColors.ButtonHighlight
-		self._label2.Location = System.Drawing.Point(301, 9)
-		self._label2.Name = "label2"
-		self._label2.Size = System.Drawing.Size(334, 52)
-		self._label2.TabIndex = 2
-		self._label2.Text = "Output"
+		self._CabnetInv.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		self._CabnetInv.Location = System.Drawing.Point(638, 9)
+		self._CabnetInv.Name = "CabnetInv"
+		self._CabnetInv.Size = System.Drawing.Size(334, 52)
+		self._CabnetInv.TabIndex = 2
+		self._CabnetInv.Text = "XBX"
+		self._CabnetInv.Click += self.Label2Click
+		# 
+		# Tspawn
+		# 
+		self._Tspawn.Enabled = True
+		self._Tspawn.Interval = 10
+		# 
+		# Cabnet
+		# 
+		self._Cabnet.BackColor = System.Drawing.SystemColors.AppWorkspace
+		self._Cabnet.ForeColor = System.Drawing.Color.Black
+		self._Cabnet.Location = System.Drawing.Point(544, 9)
+		self._Cabnet.Name = "Cabnet"
+		self._Cabnet.RightToLeft = System.Windows.Forms.RightToLeft.No
+		self._Cabnet.Size = System.Drawing.Size(88, 40)
+		self._Cabnet.TabIndex = 3
+		self._Cabnet.Text = "Medical Supplies"
+		self._Cabnet.Click += self.CabnetClick
+		# 
+		# Radio
+		# 
+		self._Radio.BackColor = System.Drawing.SystemColors.AppWorkspace
+		self._Radio.ForeColor = System.Drawing.Color.Black
+		self._Radio.Location = System.Drawing.Point(884, 90)
+		self._Radio.Name = "Radio"
+		self._Radio.RightToLeft = System.Windows.Forms.RightToLeft.No
+		self._Radio.Size = System.Drawing.Size(88, 43)
+		self._Radio.TabIndex = 4
+		self._Radio.Text = "Medical Requests"
+		self._Radio.Click += self.Label4Click
+		# 
+		# progressBar1
+		# 
+		self._progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+		self._progressBar1.Location = System.Drawing.Point(884, 64)
+		self._progressBar1.Name = "progressBar1"
+		self._progressBar1.Size = System.Drawing.Size(88, 23)
+		self._progressBar1.TabIndex = 5
+		# 
+		# DebugBox
+		# 
+		self._DebugBox.BackColor = System.Drawing.SystemColors.ButtonFace
+		self._DebugBox.Location = System.Drawing.Point(366, 105)
+		self._DebugBox.Name = "DebugBox"
+		self._DebugBox.Size = System.Drawing.Size(100, 23)
+		self._DebugBox.TabIndex = 6
+		self._DebugBox.Text = "DebugBox"
 		# 
 		# MainForm
 		# 
 		self.BackColor = System.Drawing.SystemColors.ActiveCaptionText
 		self.ClientSize = System.Drawing.Size(984, 461)
-		self.Controls.Add(self._label2)
+		self.Controls.Add(self._DebugBox)
+		self.Controls.Add(self._progressBar1)
+		self.Controls.Add(self._Radio)
+		self.Controls.Add(self._Cabnet)
+		self.Controls.Add(self._CabnetInv)
 		self.Controls.Add(self._player)
 		self.Controls.Add(self._label1)
 		self.Name = "MainForm"
@@ -66,7 +122,11 @@ class MainForm(Form):
 		self.KeyUp += self.MainFormKeyUp
 		self.ResumeLayout(False)
 
+	def Proximity(self, X1, X2, Y1, Y2): 
+		d = math.sqrt(((X2-X1)**2) + ((Y2-Y1)**2))
+		self._DebugBox.Text = str(d)
 
+		
 
 	def TscrollTick(self, sender, e):
 		
@@ -89,9 +149,6 @@ class MainForm(Form):
 		pass
 
 	def MainFormKeyDown(self, sender, e):
-		
-		
-		
 				
 		if e.KeyCode == Keys.A:
 			self.playflagleft = True 
@@ -114,5 +171,19 @@ class MainForm(Form):
 		elif e.KeyCode == Keys.S:
 			self.playflagdown  = False 
 			pass
+		
+		pass
+	
+	
 
+	def Label2Click(self, sender, e):
+		self._label2.Text = "Clicked" 
+		
+		pass
+
+	def Label4Click(self, sender, e):
+		pass
+
+	def CabnetClick(self, sender, e):
+		return self.Proximity(self._player.Top+32, self._Cabnet.Bottom-20, self._player.Right-16, self._Cabnet.Left+40)
 		pass
