ace7c991a818e9b019d42e6c68038e598f2b4be3
zacryecrowe <crowe.z@stu.janesville.k12.wi.us>
2024-01-15 18:07:29 -0600

    sdasdasd
    asdasdasd

--- ProgrammingI_Final/MainForm.py
+++ ProgrammingI_Final/MainForm.py
@@ -6,7 +6,7 @@ from System.Windows.Forms import *
 from Menus import * 
 from Menus2 import * 
 from store import * 
-from Patient import *
+from Patients import * 
 class MainForm(Form):
 	def __init__(self):
 		self.InitializeComponent()
@@ -24,9 +24,13 @@ class MainForm(Form):
 		self.counter = 0
 		self.delivertime = 45 
 		#Center of Objects for Prox function. Each is first 3 letters of word with loc after
+		self.playloc = [self._player.Top+32, self._player.Right-16]
 		self.cabloc = [self._Cabnet.Bottom-25, self._Cabnet.Right-50]
-		self.radloc = [self._Radio.Bottom-25, self._Radio.Right-50]		
-		###
+		self.radloc = [self._Radio.Bottom-25, self._Radio.Right-50]
+		#self.bed1loc = [self._Bed1.Top+44, self._Bed1.Right-36]		
+		###### All Patient Stuff ######## 
+		
+		patients = []
 		
 		self.menu = Menus(self)
 		self.menu2 = Menus2(self)
@@ -48,12 +52,13 @@ class MainForm(Form):
 		self._UpdateTimer = System.Windows.Forms.Timer(self._components)
 		self._playerinv = System.Windows.Forms.Label()
 		self._TrequestTimer = System.Windows.Forms.Timer(self._components)
+		self._Bed2 = System.Windows.Forms.Label()
+		self._button1 = System.Windows.Forms.Button()
+		self._progressBar2 = System.Windows.Forms.ProgressBar()
+		self._label2 = System.Windows.Forms.Label()
+		self._TBattle = System.Windows.Forms.Timer(self._components)
 		self._label3 = System.Windows.Forms.Label()
-		self._label5 = System.Windows.Forms.Label()
-		self._label6 = System.Windows.Forms.Label()
-		self._label7 = System.Windows.Forms.Label()
-		self._label8 = System.Windows.Forms.Label()
-		self._label9 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
 		self.SuspendLayout()
 		# 
 		# label1
@@ -67,7 +72,7 @@ class MainForm(Form):
 		# player
 		# 
 		self._player.BackColor = System.Drawing.SystemColors.ActiveBorder
-		self._player.Location = System.Drawing.Point(416, 236)
+		self._player.Location = System.Drawing.Point(416, 198)
 		self._player.Name = "player"
 		self._player.Size = System.Drawing.Size(28, 67)
 		self._player.TabIndex = 1
@@ -87,7 +92,6 @@ class MainForm(Form):
 		self._Shelf.Size = System.Drawing.Size(334, 23)
 		self._Shelf.TabIndex = 2
 		self._Shelf.Text = "XBX"
-		self._Shelf.Click += self.Label2Click
 		# 
 		# Tspawn
 		# 
@@ -133,9 +137,9 @@ class MainForm(Form):
 		# DebugBox
 		# 
 		self._DebugBox.BackColor = System.Drawing.SystemColors.ButtonFace
-		self._DebugBox.Location = System.Drawing.Point(372, 429)
+		self._DebugBox.Location = System.Drawing.Point(712, 279)
 		self._DebugBox.Name = "DebugBox"
-		self._DebugBox.Size = System.Drawing.Size(260, 23)
+		self._DebugBox.Size = System.Drawing.Size(260, 162)
 		self._DebugBox.TabIndex = 6
 		self._DebugBox.Text = "Dialouge Box"
 		# 
@@ -165,7 +169,7 @@ class MainForm(Form):
 		# playerinv
 		# 
 		self._playerinv.BackColor = System.Drawing.SystemColors.ButtonShadow
-		self._playerinv.Location = System.Drawing.Point(416, 266)
+		self._playerinv.Location = System.Drawing.Point(416, 228)
 		self._playerinv.Name = "playerinv"
 		self._playerinv.Size = System.Drawing.Size(28, 23)
 		self._playerinv.TabIndex = 9
@@ -176,76 +180,78 @@ class MainForm(Form):
 		self._TrequestTimer.Interval = 1000
 		self._TrequestTimer.Tick += self.TrequestTimerTick
 		# 
+		# Bed2
+		# 
+		self._Bed2.BackColor = System.Drawing.SystemColors.AppWorkspace
+		self._Bed2.ForeColor = System.Drawing.Color.Black
+		self._Bed2.Location = System.Drawing.Point(137, 177)
+		self._Bed2.Name = "Bed2"
+		self._Bed2.RightToLeft = System.Windows.Forms.RightToLeft.No
+		self._Bed2.Size = System.Drawing.Size(70, 86)
+		self._Bed2.TabIndex = 11
+		self._Bed2.Text = "Bed1"
+		# 
+		# button1
+		# 
+		self._button1.Location = System.Drawing.Point(26, 418)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(195, 34)
+		self._button1.TabIndex = 12
+		self._button1.Text = "Patient Menu"
+		self._button1.UseVisualStyleBackColor = True
+		# 
+		# progressBar2
+		# 
+		self._progressBar2.Location = System.Drawing.Point(333, 418)
+		self._progressBar2.Name = "progressBar2"
+		self._progressBar2.Size = System.Drawing.Size(363, 34)
+		self._progressBar2.TabIndex = 13
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.SystemColors.ButtonFace
+		self._label2.Location = System.Drawing.Point(227, 418)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(100, 34)
+		self._label2.TabIndex = 14
+		self._label2.Text = "Battle Progress"
+		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# TBattle
+		# 
+		self._TBattle.Enabled = True
+		self._TBattle.Interval = 1000
+		# 
 		# label3
 		# 
-		self._label3.BackColor = System.Drawing.SystemColors.Info
-		self._label3.Location = System.Drawing.Point(297, 132)
+		self._label3.BackColor = System.Drawing.SystemColors.ControlLightLight
+		self._label3.Location = System.Drawing.Point(213, 9)
 		self._label3.Name = "label3"
-		self._label3.Size = System.Drawing.Size(72, 87)
-		self._label3.TabIndex = 11
-		self._label3.Text = "bed2"
-		self._label3.Click += self.Label3Click
+		self._label3.Size = System.Drawing.Size(21, 23)
+		self._label3.TabIndex = 15
+		self._label3.Text = "0"
+		self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
 		# 
-		# label5
+		# label4
 		# 
-		self._label5.BackColor = System.Drawing.SystemColors.Info
-		self._label5.Location = System.Drawing.Point(617, 297)
-		self._label5.Name = "label5"
-		self._label5.Size = System.Drawing.Size(72, 87)
-		self._label5.TabIndex = 15
-		self._label5.Text = "bed6"
-		self._label5.Click += self.Label5Click
-		# 
-		# label6
-		# 
-		self._label6.BackColor = System.Drawing.SystemColors.Info
-		self._label6.Location = System.Drawing.Point(450, 297)
-		self._label6.Name = "label6"
-		self._label6.Size = System.Drawing.Size(72, 87)
-		self._label6.TabIndex = 14
-		self._label6.Text = "bed5"
-		self._label6.Click += self.Label6Click
-		# 
-		# label7
-		# 
-		self._label7.BackColor = System.Drawing.SystemColors.Info
-		self._label7.Location = System.Drawing.Point(297, 297)
-		self._label7.Name = "label7"
-		self._label7.Size = System.Drawing.Size(72, 87)
-		self._label7.TabIndex = 13
-		self._label7.Text = "bed4"
-		self._label7.Click += self.Label7Click
-		# 
-		# label8
-		# 
-		self._label8.BackColor = System.Drawing.SystemColors.Info
-		self._label8.Location = System.Drawing.Point(144, 297)
-		self._label8.Name = "label8"
-		self._label8.Size = System.Drawing.Size(72, 87)
-		self._label8.TabIndex = 17
-		self._label8.Text = "bed2"
-		self._label8.Click += self.Label8Click
-		# 
-		# label9
-		# 
-		self._label9.BackColor = System.Drawing.SystemColors.Info
-		self._label9.Location = System.Drawing.Point(144, 132)
-		self._label9.Name = "label9"
-		self._label9.Size = System.Drawing.Size(72, 87)
-		self._label9.TabIndex = 20
-		self._label9.Text = "bed1"
-		self._label9.Click += self.Label9Click
+		self._label4.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		self._label4.Location = System.Drawing.Point(120, 9)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(87, 23)
+		self._label4.TabIndex = 16
+		self._label4.Text = "Patients Waiting: "
+		self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
 		# 
 		# MainForm
 		# 
 		self.BackColor = System.Drawing.SystemColors.ActiveCaptionText
 		self.ClientSize = System.Drawing.Size(984, 461)
-		self.Controls.Add(self._label9)
-		self.Controls.Add(self._label8)
-		self.Controls.Add(self._label5)
-		self.Controls.Add(self._label6)
-		self.Controls.Add(self._label7)
+		self.Controls.Add(self._label4)
 		self.Controls.Add(self._label3)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._progressBar2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._Bed2)
 		self.Controls.Add(self._playerinv)
 		self.Controls.Add(self._Shelf3)
 		self.Controls.Add(self._Shelf2)
@@ -258,7 +264,6 @@ class MainForm(Form):
 		self.Controls.Add(self._label1)
 		self.Name = "MainForm"
 		self.Text = "ProgrammingI_Final"
-		self.Load += self.MainFormLoad
 		self.KeyDown += self.MainFormKeyDown
 		self.KeyUp += self.MainFormKeyUp
 		self.ResumeLayout(False)
@@ -273,9 +278,16 @@ class MainForm(Form):
 		self.playflagup = False 
 		self.playflagright = False 
 		self.playflagdown  = False 
+		
+		
+	def MainFormKeyDown(self, sender, e):
+		if e.KeyCode == Keys.A:
+			self.playflagleft = True 
+		pass
+
+	def MainFormKeyUp(self, sender, e):
 		pass
 		
-
 	def TscrollTick(self, sender, e):
 		if self.menuflag == False: 
 			if self.playflagleft == True: 
@@ -308,50 +320,14 @@ class MainForm(Form):
 		elif self._player.Left >= 950:
 			self._player.Left -= 3
 			self._playerinv.Left -= 3
-
-	def MainFormLoad(self, sender, e):
-		pass
-
-	def MainFormKeyDown(self, sender, e):
-				
-		if e.KeyCode == Keys.A:
-			self.playflagleft = True 
-		elif e.KeyCode == Keys.W:
-			self.playflagup = True 
-		elif e.KeyCode == Keys.D:
-			self.playflagright = True 
-		elif e.KeyCode == Keys.S:
-			self.playflagdown  = True 
-			pass
-		elif e.KeyCode == Keys.R: 
-			self.store.bandrequest()
-
-	def MainFormKeyUp(self, sender, e):
-		
-		if e.KeyCode == Keys.A:
-			self.playflagleft = False 
-		elif e.KeyCode == Keys.W:
-			self.playflagup = False 
-		elif e.KeyCode == Keys.D:
-			self.playflagright = False 
-		elif e.KeyCode == Keys.S:
-			self.playflagdown  = False 
-			pass
-		
-		pass
-	
-	
-		pass
-
-
+			
 	def Label4Click(self, sender, e):
 		#Radio click 
 		dist = self.Proximity(self._player.Top+32, self.radloc[0], self._player.Right-16, self.radloc[1])
 		if dist <= self.intdist:
 			self.menu2.Show()
 			self.menuflag = True 
-		pass
-
+			
 	############################ CABNET FUNCTIONS ###########################################
 
 	def CabnetClick(self, sender, e):
@@ -361,16 +337,16 @@ class MainForm(Form):
 			self.menuflag = True 
 		else:
 			self._DebugBox.Text = "Too far away!" 
-		pass
+			
 	def Cab1Click(self):
 		self.store.band_to_player()
-		pass
+		
 	def Cab2Click(self):
 		self.store.morp_to_player()
-		pass
+		
 	def Cab3Click(self):
 		self.store.spli_to_player()
-		pass
+		
 	def Cab4Click(self):
 		self.store.heldreturn()
 		
@@ -448,25 +424,68 @@ class MainForm(Form):
 		self._TrequestTimer.Enabled = False
 		self._ProgressBar1.Increment(-30)
 		pass
-#Bed 
-	def Label2Click(self, sender, e):
-		self._label2.Text = "Clicked" 
-#Bed1		
-	def Label9Click(self, sender, e):
-		pass
-	#Bed2
-	def Label3Click(self, sender, e):
-		pass
+########## BED NONSENSE #################### 
 
-#Bed3	
-	def Label8Click(self, sender, e):
-		pass
-#Bed4
-	def Label7Click(self, sender, e):
-		pass
-#Bed5
-	def Label6Click(self, sender, e):
-		pass
-#Bed6
-	def Label5Click(self, sender, e):
-		pass
+########## PATIENT NONSENSE #################### 
+
+class Patient:
+    def __init__(self, name, rank, wounds, ):
+        self.name = name
+        self.rank = rank
+        self.wounds = wounds
+      
+    def getName(self):
+        return self.name
+
+    def getRank(self):
+        return self.rank
+
+    def getWounds(self):
+        return self.wounds
+
+    def getRandomName():
+        names = ["S.Franciszek", "Y.Dor","N.Jozef","S.Edmund","G.Gallagher","H.Iago",
+                 "A.Vitaliy","O.Langdon","E.Bertrand","I.Dayton","J.Osborne","R.Herschel",
+                 "O.Wright","T.Graciano","J.Goodwin","M.Hallsteinn","M.O'Bren"]
+        return random.choice(names)
+
+    def getRandomRank():
+        ranks = ["Private", "Corporal","Sergeant","PFC.","W.O."]
+        return random.choice(ranks)
+
+    def getRandomWounds():
+      ### Possible Wounds 
+      Gunshot = ["Gunshot-","Band",7] 
+      Shrapnel = ["Shrapnel","Band",5]
+      Scrapes = ["Scrapes","Band",3]
+      Shock = ["Shock","Morp",7]
+      Fractures = ["Fractures","Spli",2]
+      Broken_Bones = ["Broken_Bones","Spli",5]
+      Mangled_Limb = ["Mangled_Limb","Surg",10]
+      Wounds = [] 
+      Pos = [Gunshot, Shrapnel,Scrapes,Shock,Fractures,Broken_Bones,Mangled_Limb]
+      Rand = [1,2,3,4,5] 
+      num = random.choice(Rand)
+      for i in range (num): 
+        Wounds.append(random.choice(Pos))
+      return Wounds
+
+    def createRandomPatient():
+        name = Patient.getRandomName()
+        rank = Patient.getRandomRank()
+        wounds = Patient.getRandomWounds()
+        return Patient(name, rank, wounds,)
+
+
+
+
+#### TEST STUFF###
+#num_patients = 4
+
+#for _ in range(num_patients):
+ #   patients.append(Patient.createRandomPatient())
+
+
+
+#for patient in patients:
+   # print(f"Name: {patient.getName()}, Rank: {patient.getRank()}, Wounds: {patient.getWounds()}")
